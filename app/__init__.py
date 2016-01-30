da082ce212d2c67ccdb6627974cde0b00cae50fa
Craig McLaren <craigh89@yahoo.com>
2016-01-30 13:04:38 -0500

    testing 8

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://cisxaeieaglpsg:20zFt6bxOCf6O
 app.config['SECRET_KEY'] = 'GGSB216737128edsgudsgdf #$$%^&89WEG{UDF}'
 
 # app.config['UPLOAD_FOLDER'] = '/home/rascal/Documents/WebDev2/flask/project1_2/app/static/img'
-app.config['UPLOAD_FOLDER'] = 'app/static/img'
+app.config['UPLOAD_FOLDER'] = '/app/static/img'
 
 db = SQLAlchemy(app)
 from app import views, models
