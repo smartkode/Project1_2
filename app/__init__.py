a43068498e8cc61003270e1bc1dd7107187c6357
Craig McLaren <craigh89@yahoo.com>
2016-01-30 12:24:53 -0500

    testing 2

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///test'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///test'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://cisxaeieaglpsg:20zFt6bxOCf6OFLtBed5Ic7PGO@ec2-54-235-152-114.compute-1.amazonaws.com:5432/dc8edhbspjp0ri'
 app.config['SECRET_KEY'] = 'GGSB216737128edsgudsgdf #$$%^&89WEG{UDF}'
 
-app.config['UPLOAD_FOLDER'] = '/home/rascal/Documents/WebDev2/flask/project1_2/app/static/img'
+# app.config['UPLOAD_FOLDER'] = '/home/rascal/Documents/WebDev2/flask/project1_2/app/static/img'
+app.config['UPLOAD_FOLDER'] = 'app/static/img'
 
 db = SQLAlchemy(app)
 from app import views, models
