79ba99420514aaedc5852946e77450656c8a4a8d
Craig McLaren <craigh89@yahoo.com>
2016-02-03 09:45:45 -0500

    test save2

--- app/views.py
+++ app/views.py
@@ -44,7 +44,7 @@ def profile():
             if file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
                 # return os.path.abspath('.')
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
                 return "works"
                 # firstname = request.form['firstname']
                 # lastname = request.form['lastname']
