b55c49accb7e5e2b4a418acab760915d9529f645
Craig McLaren <craigh89@yahoo.com>
2016-02-03 09:58:19 -0500

    test save5

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///test'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://cvxtzukvylnave:sobDuj6be8BSex39QuKXQUEO9u@ec2-107-20-242-191.compute-1.amazonaws.com:5432/d657nrpn9952n2'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://fbtbetciuxdeyf:rGyaL_SqUh6LzsVew96JRXeIjh@ec2-54-83-61-45.compute-1.amazonaws.com:5432/d68irbpleakdl7'
 app.config['SECRET_KEY'] = 'GGSB216737128edsgudsgdf #$$%^&89WEG{UDF}'
 
 # app.config['UPLOAD_FOLDER'] = '/home/rascal/Documents/WebDev2/flask/project1_2/app/static/img'
