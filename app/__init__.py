4e63b7d6eed9bce649429bcf47e8ac26a4e9f308
Craig McLaren <craigh89@yahoo.com>
2016-02-29 20:21:13 -0500

    db uri

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://fbtbetciuxdeyf:rGyaL_SqUh6LzsVew96JRXeIjh@ec2-54-83-61-45.compute-1.amazonaws.com:5432/d68irbpleakdl7'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://twvdtrzwqnioal:e2fXsAriasw3ZrvQwO9Sz5U4Q1@ec2-107-20-148-211.compute-1.amazonaws.com:5432/d3mncmq10ulio1'
 app.config['SECRET_KEY'] = 'GGSB216737128edsgudsgdf #$$%^&89WEG{UDF}'
 app.config['UPLOAD_FOLDER'] = "app/static"
 
